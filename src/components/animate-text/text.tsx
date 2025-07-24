2ccadf1b204061780621db79a9dc58b428a9138f
Shahbozbek3002 <shahbozbeknomonjonov2000@gmail.com>
2025-07-24 12:14:12 +0500

    some changes

--- src/components/animate-text/text.tsx
+++ src/components/animate-text/text.tsx
@@ -1,3 +1,4 @@
+import { useViewportSize } from "@mantine/hooks";
 import { motion } from "framer-motion";
 
 interface AnimatedZoomTextProps {
@@ -9,6 +10,8 @@ export const AnimatedZoomText = ({
   textParts,
   className,
 }: AnimatedZoomTextProps) => {
+  const { width } = useViewportSize();
+
   return (
     <span
       className={className}
@@ -28,11 +31,15 @@ export const AnimatedZoomText = ({
                 duration: 0.35,
                 ease: [0.25, 0.8, 0.25, 1],
               }}
-              style={{
-                // display: "inline-block",
-                // whiteSpace: "pre",
-                willChange: "transform, opacity",
-              }}
+              style={
+                width < 576
+                  ? { willChange: "transform, opacity" }
+                  : {
+                      display: "inline-block",
+                      whiteSpace: "pre",
+                      willChange: "transform, opacity",
+                    }
+              }
             >
               {char}
             </motion.span>
